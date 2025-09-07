8a24af8fb0aa508fe32b6076bcf4d0a23e9196c2
Solveyga <azbite@mail.ru>
2025-09-07 15:19:43 +0300

    add browser_settings fixture

--- test_search.py
+++ test_search.py
@@ -1,6 +1,14 @@
+import pytest
 from selene import browser, be, have
 
-def test_search():
+
+@pytest.fixture
+def browser_settings():
+    browser.config.window_width = 800
+    browser.config.window_height = 600
     browser.open('https://duckduckgo.com')
+
+
+def test_search(browser_settings):
     browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
     browser.element('html').should(have.text('qa.guru - Курсы тестировщиков'))
